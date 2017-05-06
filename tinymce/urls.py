a1d8d3d470fa65ed3ce3d7524b7ae13d9995a3e9
dlancer <dmdpost@gmail.com>
2017-05-07 00:03:52 +0500

    Django v1.11 support

--- tinymce/urls.py
+++ tinymce/urls.py
@@ -3,15 +3,33 @@
 
 try:
     from django.conf.urls import url, patterns
+    v110plus = False
 except:
-    from django.conf.urls.defaults import *
+    try:
+        from django.conf.urls.defaults import *
+        v110plus = False
+    except:
+        from django.conf.urls import url
+        v110plus = True
 
-urlpatterns = patterns('tinymce.views',
-    url(r'^js/textareas/(?P<name>.+)/$', 'textareas_js', name='tinymce-js'),
-    url(r'^js/textareas/(?P<name>.+)/(?P<lang>.*)$', 'textareas_js', name='tinymce-js-lang'),
-    url(r'^spellchecker/$', 'spell_check'),
-    url(r'^flatpages_link_list/$', 'flatpages_link_list'),
-    url(r'^compressor/$', 'compressor', name='tinymce-compressor'),
-    url(r'^filebrowser/$', 'filebrowser', name='tinymce-filebrowser'),
-    url(r'^preview/(?P<name>.+)/$', 'preview', name='tinymce-preview'),
-)
+if v110plus:
+    from . import views
+    urlpatterns = [
+        url(r'^js/textareas/(?P<name>.+)/$', views.textareas_js, name='tinymce-js'),
+        url(r'^js/textareas/(?P<name>.+)/(?P<lang>.*)$', views.textareas_js, name='tinymce-js-lang'),
+        url(r'^spellchecker/$', views.spell_check),
+        url(r'^flatpages_link_list/$', views.flatpages_link_list),
+        url(r'^compressor/$', views.compressor, name='tinymce-compressor'),
+        url(r'^filebrowser/$', views.filebrowser, name='tinymce-filebrowser'),
+        url(r'^preview/(?P<name>.+)/$', views.preview, name='tinymce-preview'),
+    ]
+else:
+    urlpatterns = patterns('tinymce.views',
+        url(r'^js/textareas/(?P<name>.+)/$', 'textareas_js', name='tinymce-js'),
+        url(r'^js/textareas/(?P<name>.+)/(?P<lang>.*)$', 'textareas_js', name='tinymce-js-lang'),
+        url(r'^spellchecker/$', 'spell_check'),
+        url(r'^flatpages_link_list/$', 'flatpages_link_list'),
+        url(r'^compressor/$', 'compressor', name='tinymce-compressor'),
+        url(r'^filebrowser/$', 'filebrowser', name='tinymce-filebrowser'),
+        url(r'^preview/(?P<name>.+)/$', 'preview', name='tinymce-preview'),
+    )
